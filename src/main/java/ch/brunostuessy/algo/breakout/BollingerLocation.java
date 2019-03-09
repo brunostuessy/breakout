1af2de57b08c159666b9030b5c6fb3181e64a005
brunostuessy <bruno.stuessi@gnail.com>
2019-03-09 18:37:31 +0100

    Added enum BollingerLocation.

---
+++ src/main/java/ch/brunostuessy/algo/breakout/BollingerLocation.java
@@ -0,0 +1,13 @@
+package ch.brunostuessy.algo.breakout;
+
+/**
+ * The location relative to a Bollinger Band.
+ * 
+ * @author Bruno Stüssi
+ *
+ */
+public enum BollingerLocation {
+
+	ABOVEUPPER(), ABOVEMIDDLE(), ONMIDDLE(), BELOWMIDDLE(), BELOWLOWER()
+
+}
