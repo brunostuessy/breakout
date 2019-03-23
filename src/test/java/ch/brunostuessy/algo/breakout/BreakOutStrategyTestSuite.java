cf6dfc320db76aabfc405aa0d94f80db8e9e6e42
brunostuessy <bruno.stuessi@gnail.com>
2019-03-23 17:04:32 +0100

    Disabled test suite.

--- src/test/java/ch/brunostuessy/algo/breakout/BreakOutStrategyTestSuite.java
+++ src/test/java/ch/brunostuessy/algo/breakout/BreakOutStrategyTestSuite.java
@@ -1,11 +1,11 @@
 package ch.brunostuessy.algo.breakout;
 
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.runner.RunWith;
+//import org.junit.platform.runner.JUnitPlatform;
+//import org.junit.platform.suite.api.SelectClasses;
+//import org.junit.runner.RunWith;
 
-@RunWith(JUnitPlatform.class)
-@SelectClasses({ BreakOutStrategyTest.class, BreakOutStrategySimulatorTest.class })
+//@RunWith(JUnitPlatform.class)
+//@SelectClasses({ BreakOutStrategyTest.class, BreakOutStrategySimulatorTest.class })
 public class BreakOutStrategyTestSuite {
 
 }
